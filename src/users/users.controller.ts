488a9168fbda9b0f08a4c13819ed1d981ffe564f
yatish1309 <myrk1309@gmail.com>
2025-04-10 21:58:39 +0530

    added accessing the specific param/query

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,20 @@
-import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Headers, Param, Post, Query } from "@nestjs/common";
 
 //http:localhost:3000/users
 @Controller('users')
 export class UsersController{
   @Get('/:id/{:optional}')
-  public getUsers(@Param() params:any,@Query() query:any){
+  public getUsers(@Param() params:any,@Query() query:any,@Param('id') id:any){
     console.log(params)
     console.log(query)
+    console.log(id)
     return 'You sent a get req to users endpoint'
   }
 
   @Post()
-  public createUsers(@Body() req:any){
+  public createUsers(@Body() req:any,@Headers() headers:any){
     console.log(req)
+    console.log(headers)
     return 'You sent a post req to users endpoint'
   }
 }
