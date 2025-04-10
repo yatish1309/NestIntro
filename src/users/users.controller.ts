bb7cfe05789f19d56a330a3f3881d9efd883ad51
yatish1309 <myrk1309@gmail.com>
2025-04-10 22:34:25 +0530

    pipes intro

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Get, Headers, Param, Post, Query } from "@nestjs/common";
+import { Body, Controller, DefaultValuePipe, Get, Headers, Param, ParseIntPipe, Post, Query } from "@nestjs/common";
 
 //http:localhost:3000/users
 @Controller('users')
 export class UsersController{
   @Get('/:id/{:optional}')
-  public getUsers(@Param() params:any,@Query() query:any,@Param('id') id:any){
+  public getUsers(@Param() params:any,@Query() query:any,@Param('id',ParseIntPipe) id:number,
+  @Query('limit',new DefaultValuePipe(10),ParseIntPipe) limit:number,
+  @Query('page', new DefaultValuePipe(1), ParseIntPipe) page:number
+){
     console.log(params)
     console.log(query)
-    console.log(id)
+    console.log(id,typeof id)
+    console.log(limit,typeof limit)
+    console.log(page,typeof page)
+
     return 'You sent a get req to users endpoint'
   }
 
